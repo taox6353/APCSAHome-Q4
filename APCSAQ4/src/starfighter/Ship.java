4e69ff415f395c0ead611905d579103975ec4096
taox6353 <taox6353@CA-SU-F106-28.sduhsd.lan>
2018-04-19 15:01:18 -0700

    Starfighter files

---
+++ APCSAQ4/src/starfighter/Ship.java
@@ -0,0 +1,65 @@
+package starfighter;
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class Ship extends MovingThing
+{
+	private int speed;
+	private Image image;
+
+	public Ship()
+	{
+		this(0,0,0);
+	}
+
+	public Ship(int x, int y)
+	{
+		this(x,y,0);
+	}
+
+	public Ship(int x, int y, int s)
+	{
+		super(x, y);
+		speed=s;
+		try
+		{
+			image = ImageIO.read(new File("src\\starfighter\\ship.jpg"));
+		}
+		catch(Exception e)
+		{
+			//feel free to do something here
+			System.out.println("File could not be found or read. ");
+		}
+	}
+
+
+	public void setSpeed(int s)
+	{
+	   //add more code
+		speed = s;
+	}
+
+	public int getSpeed()
+	{
+	   return speed;
+	}
+
+	public void draw( Graphics window )
+	{
+   	window.drawImage(image,getX(),getY(),80,80,null);
+	}
+
+	public String toString()
+	{
+		return super.toString() + getSpeed();
+	}
+}
